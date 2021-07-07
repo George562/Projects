5d1682f5a890c9a78f1da61ffd343ecf7ef9c081
George562 <51839473+George562@users.noreply.github.com>
2021-07-07 11:01:19 +0300

    Add files via upload

--- game_in_life/game_in_life.cpp
+++ game_in_life/game_in_life.cpp
@@ -2,13 +2,17 @@
 #include <vector>
 #include <iostream>
 // -lsfml-graphics -lsfml-window -lsfml-system
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
 
-int n = 64, size = 14, x, y;
+int n = 256, size = 3, x, y;
+int filling = 0, scs = n * size;
+bool drawing = false, pause = true;
 
-std::vector<std::vector<int>> dudes(n, std::vector<int>(n, 0));
-std::vector<std::vector<int>> new_dudes(n, std::vector<int>(n, 0));
+vvi dudes(n, vi(n, 0));
+vvi change(n * n, vi(2, 0));
 
-std::vector<std::vector<int>> colors = {
+vvi colors = {
     {200, 200, 200}, {255,   0,   0},  // white, red
     {255, 255, 255}, {255, 100, 100},  // lwhite, lred
     {100, 100, 100}, {150,   0,   0}   // gray, bred
@@ -17,74 +21,39 @@ std::vector<std::vector<int>> colors = {
 sf::RectangleShape rect1(sf::Vector2f(size, size));
 sf::RectangleShape rect2(sf::Vector2f(size, size / 7));
 sf::ConvexShape convex;
+sf::Mouse Mouse;
 
-std::vector<sf::RectangleShape> rects1(n * n, sf::RectangleShape(sf::Vector2f(size, size)));
-std::vector<sf::RectangleShape> rects2(n * n, sf::RectangleShape(sf::Vector2f(size, size / 7)));
-std::vector<sf::ConvexShape> convexes(n * n);
-
-sf::RenderWindow window(sf::VideoMode(n * size, n * size), "game in life");
-
-// sf::Texture alive_texture, dead_texture;
-// sf::Sprite alive, dead;
+sf::RenderWindow window(sf::VideoMode(scs, scs), "game in life");
 
 int find(int x, int y);
-
 void draw(int x, int y, int c);
-
-void play();
+void step();
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    // window.setVerticalSyncEnabled(true);
-    for (int y = 0; y < n; y++)
-        for (int x = 0; x < n; x++) {
-            rects1[y * n + x].setPosition(x * size, y * size);
-            rects2[y * n + x].setPosition(x * size, y * size);
 
-            convexes[y * n + x].setPointCount(4);
-            convexes[y * n + x].setPoint(0, sf::Vector2f(x * size, y * size));
-            convexes[y * n + x].setPoint(1, sf::Vector2f(x * size, y * size + size));
-            convexes[y * n + x].setPoint(2, sf::Vector2f(x * size + size / 7, y * size + size));
-            convexes[y * n + x].setPoint(3, sf::Vector2f(x * size + size / 7, y * size + size / 7));
-            draw(x, y, 0);
-        }
     convex.setPointCount(4);
-    // alive_texture.loadFromFile("alive.png");
-    // dead_texture.loadFromFile("dead.png");
-    // alive.setTexture(alive_texture); dead.setTexture(dead_texture);
-    // alive.setTextureRect(sf::IntRect(10, 10, 32, 32));
-    // alive.setScale(size, size); dead.setScale(size, size);
-
-    bool drawing = false, pause = true;
-    int filling = 0;
 
     sf::Clock clock;
     sf::Time time = clock.getElapsedTime();
 
-    // for (int y = 0; y < n; y++)
-    //     for (int x = 0; x < n; x++)
-    //         draw(x, y, 0);
-
     while (window.isOpen()) {
         if (drawing) {
-            sf::Vector2i mouse = sf::Mouse::getPosition(window);
-            x = mouse.x; y = mouse.y;
-            if (0 <= x && x < size * n && 0 <= y && y < size * n) {
-                draw(x / size, y / size, filling);
-                new_dudes[y / size][x / size] = filling;
-                dudes[y / size][x / size] = filling;
-            }
+            sf::Vector2i mouse = Mouse.getPosition(window);
+            x = mouse.x / size; y = mouse.y / size;
+            if (0 <= x && x < n && 0 <= y && y < n)
+                dudes[y][x] = filling;
         }
 
-        if (clock.getElapsedTime() - time >= sf::milliseconds(40)) {
+        if (clock.getElapsedTime() - time >= sf::milliseconds(50)) {
             if (!pause)
-                play();
-            
+                step();
+
+            window.clear();
             for (int y = 0; y < n; y++)
                 for (int x = 0; x < n; x++)
                     draw(x, y, dudes[y][x]);
-
             window.display();
             time = clock.getElapsedTime();
         }
@@ -93,10 +62,13 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-
+            
             if (event.type == sf::Event::KeyPressed)
-                if (event.key.code == 57)
-                    pause = !pause;
+                switch (event.key.code) {
+                    case sf::Keyboard::Space: pause = !pause; break;
+                    case sf::Keyboard::Tab: dudes.clear(); break;
+                    case sf::Keyboard::Escape: window.close(); break;
+                }
 
             if (event.type == sf::Event::MouseButtonReleased)
                 drawing = false;
@@ -111,20 +83,21 @@ int main() {
     return 0;
 }
 
-void play() {
-    int count;
-    for (int y = 0; y < n; y++)
-        for (int x = 0; x < n; x++) {
+void step() {
+    int x, y, count, cur = 0;
+    for (y = 0; y < n; y++)
+        for (x = 0; x < n; x++) {
             count = find(x, y);
-            if (count == 3) {
-                new_dudes[y][x] = 1;
-            } else if (count != 2) {
-                new_dudes[y][x] = 0;
+            if (dudes[y][x] != 1 && count == 3) {
+                change[cur][0] = y; change[cur++][1] = x;
+            } else if (dudes[y][x] == 1 && count != 2 && count != 3) {
+                change[cur][0] = y; change[cur++][1] = x;
             }
         }
-    for (int y = 0; y < n; y++)
-        for (int x = 0; x < n; x++)
-            dudes[y][x] = new_dudes[y][x];
+    for (cur = cur - 1; cur >= 0; cur--) {
+        y = change[cur][0]; x = change[cur][1];
+        dudes[y][x] = (dudes[y][x] + 1) % 2;
+        }
 }
 
 int find(int x, int y) {
@@ -136,37 +109,26 @@ int find(int x, int y) {
 }
 
 void draw(int x, int y, int c) {
-    rects1[y * n + x].setFillColor(sf::Color(colors[c][0], colors[c][1], colors[c][2]));
-    rects1[y * n + x].setFillColor(sf::Color(colors[c][0], colors[c][1], colors[c][2]));
-    window.draw(rects1[y * n + x]);
+    if (c == 0) return;
+    using namespace sf;
+    x *= size; y *= size;
 
-    c += 2;
-    rects2[y * n + x].setFillColor(sf::Color(colors[c][0], colors[c][1], colors[c][2]));
-    rects2[y * n + x].setFillColor(sf::Color(colors[c][0], colors[c][1], colors[c][2]));
-    window.draw(rects2[y * n + x]);
+    rect1.setFillColor(Color(colors[c][0], colors[c][1], colors[c][2]));
+    rect1.setPosition(x, y);
+    window.draw(rect1);
 
-    c += 2;
-    convexes[y * n + x].setFillColor(sf::Color(colors[c][0], colors[c][1], colors[c][2]));
-    convexes[y * n + x].setFillColor(sf::Color(colors[c][0], colors[c][1], colors[c][2]));
-    window.draw(convexes[y * n + x]);
-    // x *= size; y *= size;
-    // if (c) {alive.setPosition(sf::Vector2f(x, y)); window.draw(alive);}
-    // else {dead.setPosition(sf::Vector2f(x, y)); window.draw(dead);}
+    if (n <= 128) {
+        c += 2;
+        rect2.setFillColor(Color(colors[c][0], colors[c][1], colors[c][2]));
+        rect2.setPosition(x, y);
+        window.draw(rect2);
 
-    // rect1.setFillColor(sf::Color(colors[c][0], colors[c][1], colors[c][2]));
-    // rect1.setPosition(x, y);
-    // window.draw(rect1);
-
-    // c += 2;
-    // rect2.setFillColor(sf::Color(colors[c][0], colors[c][1], colors[c][2]));
-    // rect2.setPosition(x, y);
-    // window.draw(rect2);
-
-    // c += 2;
-    // convex.setFillColor(sf::Color(colors[c][0], colors[c][1], colors[c][2]));
-    // convex.setPoint(0, sf::Vector2f(x, y));
-    // convex.setPoint(1, sf::Vector2f(x, y + size));
-    // convex.setPoint(2, sf::Vector2f(x + size / 7, y + size));
-    // convex.setPoint(3, sf::Vector2f(x + size / 7, y + size / 7));
-    // window.draw(convex);
+        c += 2;
+        convex.setFillColor(Color(colors[c][0], colors[c][1], colors[c][2]));
+        convex.setPoint(0, Vector2f(x, y));
+        convex.setPoint(1, Vector2f(x, y + size));
+        convex.setPoint(2, Vector2f(x + size / 7, y + size));
+        convex.setPoint(3, Vector2f(x + size / 7, y + size / 7));
+        window.draw(convex);
+    }
 }
